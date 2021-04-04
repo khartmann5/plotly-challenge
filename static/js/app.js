295e4723629df320ac239f1504d2de8f1bc110b2
khartmann5 <hartmann.kt@gmail.com>
2021-04-03 20:07:16 -0700

    saving bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -34,20 +34,19 @@ function optionChanged(id) {
 function getPlot(id) {
     // read the data
     d3.json('samples.json').then(data => {
-        console.log(data);
+        // console.log(data);
 
         let washfreq = data.metadata.map(d => d.washfreq);
-        console.log(`Washing frequency: ${washfreq}`);
 
         let samp = data.samples.filter(s => s.id.toString() === id)[0];
-        console.log(samp);
+        // console.log(samp);
 
         // get only top 10 sample values for plot
         let sampleValues = samp.sample_values.slice(0,10).reverse();
 
         let idValues = (samp.otu_ids.slice(0,10)).reverse();
 
-        let idtu = idValues.map(d => "OTU" + d)
+        let idtu = idValues.map(d => "OTU " + d)
         console.log(idtu);
 
         let lables = samp.otu_labels.slice(0,10);
@@ -70,12 +69,34 @@ function getPlot(id) {
         // create layout
         let layout = {
             title: "Top 10 OTU",
-
+            yaxis:{tickmode:"linear"},
+            margin:{l:100,r:100,t:30,b:20}
         };
         
         // create horizontal bar plot
         Plotly.newPlot("bar", barData, layout);
         
+        let trace2 = {
+            x: samp.otu_ids,
+            y: samp.sample_values,
+            mode: "markers",
+            marker: {
+                size: samp.sample_values,
+                color: samp.otu_ids
+            },
+            text: samp.otu_labels
+        };
+
+        let layout2 = {
+            xaxis:{title: "OTU ID"},
+            height: 600,
+            width: 1300
+        };
+
+        let bubbleData = [trace2];
+
+        Plotly.newPlot("bubble", bubbleData, layout2)
+
 
 
     });
