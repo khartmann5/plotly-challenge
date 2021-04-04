3bfb95c66d08c0563f4625d5f03601a8eb716590
khartmann5 <hartmann.kt@gmail.com>
2021-04-03 19:55:27 -0700

    added bar graph

--- static/js/app.js
+++ static/js/app.js
@@ -19,10 +19,16 @@ function init() {
             dropDown.append("option").text(name).property("Value");
         });
 
-        // getPlot(data.names[0]);
+        getPlot(data.names[0]);
         // getInfo(data.names[0]);
     });
 };
+
+// create the function for the change event
+function optionChanged(id) {
+    getPlot(id);
+    getInfo(id);
+}
 
 // build function that retrieves data and creates plots
 function getPlot(id) {
@@ -38,6 +44,39 @@ function getPlot(id) {
 
         // get only top 10 sample values for plot
         let sampleValues = samp.sample_values.slice(0,10).reverse();
+
+        let idValues = (samp.otu_ids.slice(0,10)).reverse();
+
+        let idtu = idValues.map(d => "OTU" + d)
+        console.log(idtu);
+
+        let lables = samp.otu_labels.slice(0,10);
+
+        console.log(sampleValues);
+        console.log(idValues);
+
+        // create trace variable
+        let trace1 = {
+            x: sampleValues,
+            y: idtu,
+            text: lables,
+            type: "bar",
+            orientation: "h",
+        };
+        
+        // create data variable
+        let barData = [trace1]
+
+        // create layout
+        let layout = {
+            title: "Top 10 OTU",
+
+        };
+        
+        // create horizontal bar plot
+        Plotly.newPlot("bar", barData, layout);
+        
+
 
     });
 };
